19f601f7b7a5b0bda3bfe7299a24168f07cda803
edasplayz <ekrutkevicius@gmail.com>
2024-03-22 16:20:38 +0200

    dfs

--- Zombieland Invasion 2/Assets/Scrip totoriial/Character/Player/CharacterNetworkManager.cs
+++ Zombieland Invasion 2/Assets/Scrip totoriial/Character/Player/CharacterNetworkManager.cs
@@ -5,8 +5,9 @@ using Unity.Netcode;
 
 public class CharacterNetworkManager : NetworkBehaviour
 {
-
+    
     [Header("Position")]
+
     public NetworkVariable<Vector3> networkPosition = new NetworkVariable<Vector3>(Vector3.zero, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     public NetworkVariable<Quaternion> networkRotation = new NetworkVariable<Quaternion>(Quaternion.identity, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     public Vector3 networkPositionVelocity;
