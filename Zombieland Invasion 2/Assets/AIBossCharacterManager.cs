4346d4154037bf9c2a3ba179374fead14db57f73
edasplayz <ekrutkevicius@gmail.com>
2024-04-19 14:53:43 +0300

    fog wall

--- Zombieland Invasion 2/Assets/AIBossCharacterManager.cs
+++ Zombieland Invasion 2/Assets/AIBossCharacterManager.cs
@@ -8,13 +8,28 @@ public class AIBossCharacterManager : AICharacterManager
     // GIVE THIS A,I a unique if
     public int bossID = 0;
     [SerializeField] bool hasBeenDefeated = false;
+    [SerializeField] bool hasBeenAwakened = false;
+    [SerializeField] List<FogWallInteractable> fogWalls;
     // when this ai is spawned check our save file (dictonary)
     // if the save file does not contain a boss monster with this i.d add it
     // if it is present cheeck if boss has been defeted
     // if the boss has been defeted disable the game object
     // if the boss has not been defeted allow this object to continue to be active
 
+    [Header("Debug")]
+    [SerializeField] bool wakeBossUp = false;
 
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (wakeBossUp)
+        {
+            wakeBossUp = false;
+            WakeBoss();
+        }
+    }
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
@@ -32,16 +47,59 @@ public class AIBossCharacterManager : AICharacterManager
             else
             {
                 hasBeenDefeated = WorldSaveGameManager.instance.currentCharacterData.bossesDefeated[bossID];
+                hasBeenAwakened = WorldSaveGameManager.instance.currentCharacterData.bossesAwakened[bossID];
 
-                if(hasBeenDefeated )
+                
+            }
+
+            // locate fog wall
+            StartCoroutine(GetFogWallsFromWorldObjectManager());
+
+            // if boss has been awakened enable the fog walls
+
+            if (hasBeenAwakened)
+            {
+                for (int i = 0; i < fogWalls.Count; i++)
                 {
-                    aICharacterNetworkManager.isActive.Value = false;
+                    fogWalls[i].isActive.Value = true;
                 }
             }
+
+            // if the bos has been defeted disable fog walls
+            if (hasBeenDefeated)
+            {
+                for (int i = 0; i < fogWalls.Count; i++)
+                {
+                    fogWalls[i].isActive.Value = false;
+                }
+                aICharacterNetworkManager.isActive.Value = false;
+            }
+
+
         }
     }
 
-    
+    private IEnumerator GetFogWallsFromWorldObjectManager()
+    {
+        while (WorldObjectManager.instance.fogWalls.Count == 0)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        // locate fog wall
+        // you can ether share the same ID for boss and fog wall or simply place a fogwall ID cariable here on look for it using that
+
+        fogWalls = new List<FogWallInteractable>();
+
+        // method 1
+        foreach (var fogWall in WorldObjectManager.instance.fogWalls)
+        {
+            if (fogWall.fogWallID == bossID)
+            {
+                fogWalls.Add(fogWall);
+            }
+        }
+    }
 
     public override IEnumerator ProcessDeathEvent(bool manuallySelectDeathAnimation = false)
     {
@@ -90,6 +148,31 @@ public class AIBossCharacterManager : AICharacterManager
         // award players with runes
 
         // disable character 
+    }
+
+    public void WakeBoss()
+    {
+        hasBeenAwakened = true;
+        // if our save data does not contain information on this boss add it now 
+        if (!WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.ContainsKey(bossID))
+        {
+            WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.Add(bossID, true);
+            
+        }
+        // otherwise load the data that already exist on this boss
+        else
+        {
+            WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.Remove(bossID);
+           
+            WorldSaveGameManager.instance.currentCharacterData.bossesAwakened.Add(bossID, true);
+           
+
+        }
+
+        for (int i = 0; i < fogWalls.Count; i++)
+        {
+            fogWalls[i].isActive.Value = true;
+        }
     }
 }
 
