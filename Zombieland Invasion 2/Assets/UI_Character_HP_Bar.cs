2cb7511f618952783dfe66982555e94ff005da7b
edasplayz <ekrutkevicius@gmail.com>
2024-04-24 14:16:34 +0300

    taip

---
+++ Zombieland Invasion 2/Assets/UI_Character_HP_Bar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+//  performs identical to to ui_stat bar except this bar appers and disapers in world space (will always face camera)
+public class UI_Character_HP_Bar : UI_StatBar
+{
+    private CharacterManager character;
+
+   // [SerializeField] 
+}
